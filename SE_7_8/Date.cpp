011ab4943b055e92139ddef6aa6cbd19ef440312
amirpourastarabadi <amirpourastarabadi@gmail.com>
2022-12-11 09:14:54 +0330

    develop date class

--- SE_7_8/Date.cpp
+++ SE_7_8/Date.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -15,6 +16,11 @@ public:
         set_date(day, month, year);
     }
 
+    Date()
+    {
+        set_date(1, 1, 0);
+    }
+
     void set_date(int day, int month, int year)
     {
         if (
@@ -27,15 +33,65 @@ public:
         this->month = month;
         this->year = year;
     }
-    void get_date()
+    
+    // getters
+    string get_date() { return "" + to_string(day) + '-' + to_string(month) + '-' + to_string(year); }
+    int get_day() { return day; }
+    int get_month() { return month; }
+    int get_year() { return year; }
+
+    int diff_in_days(Date date)
     {
-        cout << day << "-" << month << "-" << year << endl;
+        int diff = 0;
+
+        while (!this->is_equals(date))
+        {
+            date.add_days();
+            diff++;
+        }
+
+        return diff;
+    }
+
+    bool is_equals(Date date)
+    {
+        return day == date.day && month == date.month && year == date.year;
+    }
+
+    void add_days(int amount = 1)
+    {
+        day++;
+        if (month >= 1 && month <= 6 && day > 31)
+        {
+            month += 1;
+            day = day % 31;
+        }
+        else if (month >= 7 && day > 30)
+        {
+            month += 1;
+            day = day % 30;
+
+            if (month > 12)
+            {
+                year += 1;
+                month %= 12;
+            }
+        }
     }
 };
 
 int main()
 {
     Date d(4, 5, 6);
-    d.get_date();
+    cout << d.get_date() << endl;
+    cout << "Day = " << d.get_day() << endl;
+    cout << "Month = " << d.get_month() << endl;
+    cout << "Year = " << d.get_year() << endl;
+
+    Date big(1, 1, 2);
+    Date small(1, 1, 1);
+    int diff = big.diff_in_days(small);
+
+    cout << "Diff days = " << diff << endl;
     return 0;
 }
