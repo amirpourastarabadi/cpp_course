70cddd528be98baecfb4845b8cc6e780b00d6708
amirpourastarabadi <amirpourastarabadi@gmail.com>
2022-12-17 18:59:21 +0330

    method overloading

--- SE_12/complex.cpp
+++ SE_12/complex.cpp
@@ -19,17 +19,38 @@ public:
     {
         return Complex(real + c.real, imag + c.imag);
     }
+
+    Complex operator+(double real)
+    {
+        return Complex(this->real + real, this->imag);
+    }
+
     Complex &operator+=(const Complex &c)
     {
         real += c.real;
         imag += c.real;
         return *this;
     }
+
+    double get_real()
+    {
+        return real;
+    }
+    double get_imag()
+    {
+        return imag;
+    }
+
     void print()
     {
         cout << "(" << to_string(real) << ", " << ((imag > 0) ? "+" : "") << to_string(imag) << "i)" << endl;
     }
 };
+
+Complex operator+(double real, Complex& c)
+{
+    return Complex(real + c.get_real(), c.get_imag());
+}
 
 int main()
 {
@@ -40,4 +61,8 @@ int main()
     a.print();
     a += c;
     a.print();   
+    c = c + 2;
+    c.print();
+    c = 2 + c;
+    c.print();
 }
