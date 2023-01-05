4ed9a04be69e7a4ff33d2ce508b9ba5d92ef1318
amirpourastarabadi <amirpourastarabadi@gmail.com>
2023-01-05 20:50:23 +0330

    first use case of template

---
+++ SE_22/templates.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class Node
+{
+public:
+    Node(T data);
+    T data;
+};
+
+template <typename T>
+Node<T>::Node(T data)
+{
+    this->data = data;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, Node<T> node)
+{
+    out << node.data;
+    return out;
+}
+
+int main()
+{
+
+    Node<int> i(10);
+    cout << i << endl;
+
+    Node<string> s("Hello template\n");
+    cout << s;
+    return 0;
+}
